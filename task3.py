07b2052c2191a36b687ba371a2d3541301064579
Heriyanto <heriyanto@xl.co.id>
2018-12-15 15:21:01 +0700

    add rename function bang

--- task3.py
+++ task3.py
@@ -1,8 +1,14 @@
-import os, os.path
+import os, sys
 
-print ("sekerip ada dih",os.getcwd())
+os.chdir("/Users/heriyanto/Downloads")
+print("sekerip ada dih",os.getcwd())
 list = os.listdir()
 jumlah_file = len(list)
 print("jumlah file dan folder",jumlah_file)
-print("isi dalemannya bang",os.listdir())
+print("isi dalemannya bang: %s"%os.listdir(os.getcwd()))
 
+for filename in os.listdir("."):
+    os.rename(filename, filename[:-4] + '1')
+
+
+print("isi dalemannya bang: %s"%os.listdir(os.getcwd()))
